a3b57fe5529f851a8a26ae55599c3eb4eadc855a
Law-Liet <a.kadirov.17.06@gmail.com>
2021-02-01 11:48:55 +0500

    static page mobile

--- resources/views/static.blade.php
+++ resources/views/static.blade.php
@@ -4,16 +4,27 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('css/bootstrap-iso.css')}}">
     <style>
+        #form_text * {
+            text-align: end;
+        }
         .panel-heading {
             background: #255dcf !important;
         }
         .panel-body {
             color: black;
         }
+        #bootstrap-iso {
+            margin: 5rem;
+        }
+        @media only screen and (max-width : 768px) {
+            #bootstrap-iso {
+                margin: 1rem;
+            }
+        }
     </style>
 @endpush
 @section('content')
-    <div id="bootstrap-iso" style="margin: 5rem">
+    <div id="bootstrap-iso">
         @include('layouts.parts.style')
             {!! setting($route) !!}
     </div>
